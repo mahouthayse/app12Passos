6f1d47d731da1df458ad8a87539005801ba4fc0a
iangYo <iangabrielrodrigues@gmail.com>
2022-01-06 21:52:26 -0300

    fix: Acessando rota não existente

--- Pages/Login/index.js
+++ Pages/Login/index.js
@@ -24,7 +24,7 @@ export default function Login() {
         try {
             const response = await api.post('/auth/login', data);
             setUserData({...userData, id: response.data._id, token: response.data.token, name: response.data.name});
-            navigate("Home");
+            navigate("AppRoutes");
         } catch (error) {
             console.log(error);
             setAlertSnackBar({
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const { Navigator, Screen } = createStackNavigator();
 
+import AppRoutes from './app.routes';
 import Login from '../Pages/Login';
 import Cadastro from '../Pages/Register';
 import ForgotPassword from "../Pages/ForgotPassword";
@@ -12,7 +13,8 @@ export default function Routes() {
     <Navigator>
       <Screen name="Login" component={Login} options={{ headerShown: false }} />
       <Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
-        <Screen name="Redefinir senha" component={ForgotPassword} options={{ headerShown: false }} />
+      <Screen name="Redefinir senha" component={ForgotPassword} options={{ headerShown: false }} />
+      <Screen name="AppRoutes" component={AppRoutes} />
     </Navigator>
   );
 }
