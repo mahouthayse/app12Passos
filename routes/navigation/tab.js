627fc92b6217933acf93dc0f53cb27489dbbd8d3
iangYo <iangabrielrodrigues@gmail.com>
2022-01-17 19:01:34 -0300

    Ícones na barra de navegação

--- routes/navigation/tab.js
+++ routes/navigation/tab.js
@@ -5,33 +5,50 @@ import Home from "../../Pages/Home";
 import Steps from "../../Pages/Steps/HomeSteps";
 import ListTestimonials from "../../Pages/Testimonials/ListTestimonials";
 import { MaterialIcons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, Entypo, FontAwesome5 } from '@expo/vector-icons';
+import { Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
+  const CustomText = ({ children }) => <Text style={{ fontSize: 11 }}>{children}</Text>;
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route, navigation }) => {
+        return {
+          tabBarActiveTintColor: '#F7B579',
+          tabBarInactiveTintColor: '#22242A',
+          headerShown: false,
+          tabBarShowLabel: true,
+          tabBarLabel: () => {
+            if (route.name === 'Home') return navigation.isFocused() ? <CustomText>Início</CustomText> : null;
+            else return navigation.isFocused() ? <CustomText>{route.name}</CustomText> : null;
+          }
+        }
+      }}
+    >
       <Tab.Screen
         name='Home'
         component={Home}
-        options={{ headerShown: false, tabBarLabel: 'Início' }}
+        options={{
+          tabBarIcon: ({ color, size }) => <Entypo name="home" size={size} color={color} />
+        }}
       />
-      <Tab.Screen name='Passos' component={Steps} options={{ headerShown: false }} />
+      <Tab.Screen 
+        name='Passos' 
+        component={Steps} 
+        options={{ tabBarIcon: ({ color, size }) => <FontAwesome5 name="shoe-prints" size={size} color={color} /> }}
+      />
       <Tab.Screen
         name='Relatos'
         component={ListTestimonials}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => <MaterialIcons name="notes" size={size} color={color} />
-        }}
+        options={{ tabBarIcon: ({ color, size }) => <MaterialIcons name="notes" size={size} color={color} /> }}
       />
       <Tab.Screen
         name='Questionário'
         component={Home}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => <AntDesign name="profile" size={size} color={color} /> }}
+        options={{ tabBarIcon: ({ color, size }) => <AntDesign name="profile" size={size} color={color} /> }}
       />
     </Tab.Navigator>
   )
