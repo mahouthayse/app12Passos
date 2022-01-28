72d70acc26667c060e6a997ff497f401309833e3
iangYo <iangabrielrodrigues@gmail.com>
2022-01-27 22:37:34 -0300

    feat: Card de perfil

--- Pages/Home/index.js
+++ Pages/Home/index.js
@@ -6,9 +6,10 @@ import {BlackText, TextPrimary, TitlePrimary, ViewPrimary} from "../../Style/glo
 import global from "../../Style/global";
 import colors from "../../Style/colors";
 import {useNavigation} from "@react-navigation/native";
-import Passos from "../../assets/passo1.png";
+import Passos from "../../assets/passo4.png";
 import Relatos from "../../assets/passo8.png";
 import Objetivo from "../../assets/passo9.png";
+import Perfil from "../../assets/passo12.png";
 import AuthContext from "../../contexts/auth";
 
 export default function Home() {
@@ -18,7 +19,7 @@ export default function Home() {
     return (
         <ViewPrimary style={{justifyContent:'space-between'}}>
             <View style={{display: 'flex', flexDirection:'row', width:'100%', justifyContent:'space-between', marginVertical:24}}>
-                <View>
+                <View style={{ marginTop: 32 }}>
                     <TitlePrimary>Olá, {userData.name.split(' ').slice(0,1).join(' ')}</TitlePrimary>
                     <TextPrimary>Você está há 100 dias sem beber.</TextPrimary>
                 </View>
@@ -29,27 +30,34 @@ export default function Home() {
                     <BlackText>23</BlackText>
                     <TextPrimary>registros no diário</TextPrimary>
                 </View>
-                <Button mode='contained' compact={true} style={global.buttonPrimary} labelStyle={global.labelPrimary} onPress={() =>navigate("Diário")}>Preencher Diário</Button>
+                <Button mode='contained' style={{ ...global.buttonPrimary, width: 164 }} labelStyle={{ ...global.labelPrimary, fontSize: 11 }} onPress={() =>navigate("Diário")}>Preencher Diário</Button>
             </View>
             <View style={{width:'100%', flexDirection:'row', alignItems:'center', justifyContent:'space-between', flexWrap:'wrap'}}>
                 <TouchableOpacity onPress={() =>navigate("Passos")}>
-                    <ImageBackground source={Passos} style={{width:160, padding:10, height:160, alignItems: 'center', justifyContent:'center',  marginVertical:16}}
+                    <ImageBackground source={Passos} style={{width:160, padding:10, height:215, alignItems: 'center', justifyContent:'center',  marginVertical:16}}
                                      imageStyle={{borderRadius:10}}>
                         <TitlePrimary style={{color: `${colors.white}`, elevation:2, fontSize:28}}>Relembre os 12 Passos</TitlePrimary>
                     </ImageBackground>
                 </TouchableOpacity>
 
                 <TouchableOpacity  onPress={() =>navigate("Relatos")}>
-                    <ImageBackground source={Relatos} style={{width:160, padding:10, height:160, alignItems: 'center', justifyContent:'center',  marginVertical:16}}
+                    <ImageBackground source={Relatos} style={{width:160, padding:10, height:215, alignItems: 'center', justifyContent:'center',  marginVertical:16}}
                                      imageStyle={{borderRadius:10}}>
                         <TitlePrimary style={{color: `${colors.white}`, elevation:2, fontSize:28}}>Relatos de Vivência</TitlePrimary>
                     </ImageBackground>
                 </TouchableOpacity>
 
                 <TouchableOpacity  onPress={() =>navigate("Objetivo")}>
-                    <ImageBackground source={Objetivo} style={{width:160, padding:10, height:160, alignItems: 'center', justifyContent:'center',  marginVertical:16}}
+                    <ImageBackground source={Objetivo} style={{width:160, padding:10, height:215, alignItems: 'center', justifyContent:'center',  marginVertical:16}}
                                      imageStyle={{borderRadius:10}}>
                         <TitlePrimary style={{color: `${colors.white}`, elevation:2, fontSize:28}}>Defina um objetivo</TitlePrimary>
+                    </ImageBackground>
+                </TouchableOpacity>
+
+                <TouchableOpacity  onPress={() => {}}>
+                    <ImageBackground source={Perfil} style={{width:160, padding:10, height:215, alignItems: 'center', justifyContent:'center',  marginVertical:16}}
+                                     imageStyle={{borderRadius:10}}>
+                        <TitlePrimary style={{color: `${colors.white}`, elevation:2, fontSize:28}}>Perfil</TitlePrimary>
                     </ImageBackground>
                 </TouchableOpacity>
             </View>
